492613ceddb86d5c8583fc44aba225e7a818ce0e
79ceca7e-1710-4439-8f8b-c115f316e9e9 <github-enterprise+79ceca7e-1710-4439-8f8b-c115f316e9e9@flatironschool.com>
2020-02-16 15:25:11 -0800

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  # attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    test_arr = [@a, @b, @c]
+    
+    if !(@a > 0 && @b > 0 && @c > 0)
+      raise TriangleError
+    elsif (@a + @b) <= @c || (@b + @c) <= @a || (@a + @c) <= @b
+      raise TriangleError
+    end
+
+
+    if test_arr.uniq.length == 1
+      :equilateral
+    elsif test_arr.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "The triangle parameters entered are illegal"
+    end
+  end
+
 end
